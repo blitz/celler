907aa467023c12b5ec9a2d75db21c3b6c5f03fdc
Julian Stecklina <julian.stecklina@cyberus-technology.de>
2026-04-14 17:18:31 +0200

    flake: remove unused docker images

--- flake/packages.nix
+++ flake/packages.nix
@@ -16,17 +16,6 @@ let
   inherit (flake-parts-lib)
     mkPerSystemOption
     ;
-
-  # Re-evaluate perSystem with cross nixpkgs
-  # HACK before https://github.com/hercules-ci/flake-parts/issues/95 is solved
-  evalCross = { system, pkgs }: config.allSystems.${system}.debug.extendModules {
-    modules = [
-      ({ config, lib, ... }: {
-        _module.args.pkgs = pkgs;
-        _module.args.self' = lib.mkForce config;
-      })
-    ];
-  };
 in
 {
   options = {
@@ -83,42 +72,6 @@ in
           };
         };
       }
-
-      (lib.mkIf pkgs.stdenv.isLinux {
-        packages = {
-          celler-server-image = pkgs.dockerTools.buildImage {
-            name = "celler-server";
-            tag = "main";
-            copyToRoot = [
-              self'.packages.celler
-
-              # Debugging utilities for `fly ssh console`
-              pkgs.busybox
-
-              # Now required by the fly.io sshd
-              pkgs.dockerTools.fakeNss
-            ];
-            config = {
-              Entrypoint = [ "/bin/cellerd" ];
-              Env = [
-                "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-              ];
-            };
-          };
-        };
-      })
-
-      (lib.mkIf (pkgs.system == "x86_64-linux") {
-        packages = {
-          celler-server-image-aarch64 = let
-            eval = evalCross {
-              system = "aarch64-linux";
-              pkgs = pkgs.pkgsCross.aarch64-multiplatform;
-            };
-
-          in eval.config.packages.celler-server-image;
-        };
-      })
     ]);
   };
 }
